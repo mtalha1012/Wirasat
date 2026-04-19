2822266770ebf937b7bfa7a303712e88ab153598
= <=>
2026-04-19 12:45:39 +0500

    go

--- schema.sql
+++ schema.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE wirasat;
 USE wirasat;
 
+CREATE TABLE asset_types(
+	type_id INT AUTO_INCREMENT,
+    category_name VARCHAR(30),
+    
+    CONSTRAINT asset_types_pk
+		PRIMARY KEY(type_id)
+    );
+    
 CREATE TABLE family_members(
     member_id INT AUTO_INCREMENT,
     cnic VARCHAR(15),
